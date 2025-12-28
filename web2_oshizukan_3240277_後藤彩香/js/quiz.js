d5759a08f9b20e4a62fdc7e3333d102283cea67a
gku2025saya <c3240277@std.gku.ac.jp>
2025-12-28 10:53:21 +0900

    Add files via upload

--- web2_oshizukan_3240277_後藤彩香/js/quiz.js
+++ web2_oshizukan_3240277_後藤彩香/js/quiz.js
@@ -3,7 +3,7 @@ const questions = [
  { text: "なとりの曲はどんな特徴がある？", choices: ["中毒性がある", "バラード中心", "元気なポップ曲", "ジャズっぽい"], correct: 0 },
  { text: "宇多田ヒカルの曲で母親への想いを歌った代表曲は何？", choices: ["Automatic", "Can You Keep A Secret?", "Traveling", "初恋"], correct: 3 },
  { text: "ちゃんみなと結婚したラッパーは誰？", choices: ["SKY-HI", "ASH ISLAND", "LEX", "R-指定"], correct: 1 },
- { text: "椎名林檎の特徴的な表現の1つは？", choices: ["単純な歌詞", "ユーモアや皮肉", "ダンス", "早口のラップ"], correct: 0 }
+ { text: "椎名林檎の特徴的な表現の1つは？", choices: ["単純な歌詞", "ユーモアや皮肉", "ダンス", "早口のラップ"], correct: 1 }
 ];
 let index = 0;
 let score = 0;
@@ -12,24 +12,6 @@ const choices = document.getElementById("choices");
 const result = document.getElementById("result");
 const nextBtn = document.getElementById("nextBtn");
 const restartBtn = document.getElementById("restartBtn");
-function showQuestion() {
- question.textContent = questions[index].text;
- choices.innerHTML = "";
- result.textContent = "";
- for (let i = 0; i < 4; i++) {
-   const btn = document.createElement("button");
-   btn.textContent = questions[index].choices[i];
-   btn.addEventListener("click", function () {
-     if (i === questions[index].correct) {
-       result.textContent = "正解！";
-       score++;
-     } else {
-       result.textContent = "不正解…";
-     }
-   });
-   choices.appendChild(btn);
- }
-}
 function nextQuestion() {
  index++;
  if (index < questions.length) {
@@ -37,12 +19,31 @@ function nextQuestion() {
  } else {
    question.textContent = "クイズ終了！";
    choices.innerHTML = "";
-   if (score >= 4) {
+   if (score === questions.length) {
      result.textContent = score + "問正解！あなたは推しマスターです！";
-   } else {
+   } else if (score >= 2 && score <= 3) {
      result.textContent = score + "問正解。もう少し推しを勉強しよう！";
+   } else {
+     result.textContent = score + "問正解。うーん、もう一度図鑑を見てみよう";
    }
  }
+}
+function nextQuestion() {
+  index++;
+  if (index < questions.length) {
+    showQuestion();
+  } else {
+    document.getElementById("nextBtn").style.display = "none";
+    question.textContent = "クイズ終了！";
+    choices.innerHTML = "";
+    if (score >= 4) {
+      result.textContent = score + "問正解！あなたは推しマスターです！";
+    } else if (score <= 2) {
+      result.textContent = score + "問正解。うーん、もう一度図鑑を見てみよう！";
+    } else if (score >= 3) {
+      result.textContent = score + "問正解。もう少し推しを勉強しよう！";
+    }
+  }
 }
 function restart() {
  index = 0;
